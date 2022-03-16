431fe2febbd73900777b77b709d8065a205a5d0a
Umar <nurbekatajonov67@gmail.com>
2022-03-16 18:28:21 +0500

    modal added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 let footerBtn = document.querySelectorAll('.footer__head-btn');
 let footerCentr = document.querySelectorAll('.footer__center');
 let backWhite = document.querySelector('.back-white');
+let modalWindow = document.querySelector('.modal__cabinet');
+let cabinetSign = document.querySelector('.sign__btn');
+let closeModal = document.querySelector('.close__modal')
 
 function footCenter(){
     backWhite.classList.toggle('active');
@@ -8,4 +11,13 @@ function footCenter(){
         footerCentr[i].classList.toggle('active')
         console.log('salom');
     }
+}
+
+function modalClose(){
+    modalWindow.classList.remove('active')
+    console.log('ex');
+}
+function modalActive(){
+    console.log('ac');
+    modalWindow.classList.add('active')
 }
