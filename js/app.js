3617f7bddea07c4c2e3eca7e44b28ae58196c532
thequlmatov <zuxroka0104@gmail.com>
2022-03-17 12:20:53 +0300

    header responsiv qilindi

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
-let footerBtn = document.querySelectorAll('.footer__head-btn');
-let footerCentr = document.querySelectorAll('.footer__center');
-let backWhite = document.querySelector('.back-white');
-let modalWindow = document.querySelector('.modal__cabinet');
-let searchInput = document.querySelector('.menu-input')
+let footerBtn = document.querySelectorAll(".footer__head-btn");
+let footerCentr = document.querySelectorAll(".footer__center");
+let backWhite = document.querySelector(".back-white");
+let modalWindow = document.querySelector(".modal__cabinet");
+let searchInput = document.querySelector(".menu-input");
 
-function footCenter(){
-    backWhite.classList.toggle('active');
-    for(i=0;i<=footerCentr.length;i++){
-        footerCentr[i].classList.toggle('active')
-    }
+function footCenter() {
+  backWhite.classList.toggle("active");
+  for (i = 0; i <= footerCentr.length; i++) {
+    footerCentr[i].classList.toggle("active");
+  }
 }
-function modalClose(){
-    modalWindow.classList.remove('active')
+function modalClose() {
+  modalWindow.classList.remove("active");
 }
-function modalActive(){
-    modalWindow.classList.add('active')
+function modalActive() {
+  modalWindow.classList.add("active");
 }
-function searchBtnOpen(){
-    searchInput.classList.add('active')
+function searchBtnOpen() {
+  searchInput.classList.add("active");
 }
+
+/* ---------------- */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+/* ----------------- */
